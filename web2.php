b981b74a6534f2e2994373ee5b44a315b1b8854a
cargarmun745 <81222258+cargarmun745@users.noreply.github.com>
2021-10-12 21:03:03 +0200

    Update web2.php

--- web2.php
+++ web2.php
@@ -4,25 +4,33 @@
     <title>web2</title>
 </head>
 <body>
+    <?php
+        require('util.php');
+        
+        enableErrors();
+    ?>
     <h2>Registro</h2>
     <form method="post" action="web3.php">
     
         <input type="hidden" name="usuario" value="<?php 
                 echo $_POST["usuario"];
-            ?>"
+            ?>" />
         
         <input type="hidden" name="contraseña1" value="<?php 
                 echo $_POST["contraseña1"];
-            ?>"
+            ?>"/>
         
         <input type="hidden" name="contraseña2" value="<?php 
                 echo $_POST["contraseña2"];
-            ?>"
-        
-        Introduce nombre: <input type="text" name="nombre"/> <br>
-        Introduce apellidos: <input type="text" name="apellidos"/> <br>
-        Introduce correo electrónico: <input type="text" name="correo"/> <br>
-        <input type="submit"/> <br>
+            ?>"/>
+            
+        <fieldset>
+            <legend>Nombre, apellidos y correo</legend><br>
+            Introduce nombre: <input type="text" name="nombre"/> <br><br>
+            Introduce apellidos: <input type="text" name="apellidos"/> <br><br>
+            Introduce correo electrónico: <input type="email" name="correo"/> <br><br>
+            <input type="submit"/> <br>
+        </fieldset>
     </form>
 </body>
 </html>
