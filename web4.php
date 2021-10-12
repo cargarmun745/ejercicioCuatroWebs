192931c762ee1efff445e44711d6a4df1a17b1b2
cargarmun745 <81222258+cargarmun745@users.noreply.github.com>
2021-10-12 21:02:23 +0200

    Update web4.php

--- web4.php
+++ web4.php
@@ -1,15 +1,27 @@
-<h2>Registro</h2>
+<h2>Registro</h2></br>
 <?php
     require('util.php');
-
+    
     enableErrors();
-
-    echo 'El usuario introducido es:' . $_POST["usuario"];?></br>
-    <?php echo 'La contraseña primera introducido es:' . $_POST["contraseña1"];?></br>
-    <?php echo 'La contraseña segunda introducido es:' . $_POST["contraseña2"];?></br>
-    <?php iguales($_POST["contraseña1"], $_POST["contraseña2"]);?></br>
-    <?php echo 'El nombre introducido es:' . $_POST["nombre"];?></br>
-    <?php echo 'Los apellidos introducidas son:' . $_POST["apellidos"];?></br>
-    <?php echo 'El correo introducido es:' . $_POST["correo"];?></br>
-    <?php echo 'Las marcas introducidas son:' . prettyVardump($_POST["marcas"]);
-?>
+    ?>
+    <fieldset>
+        <legend>Usuario y contraseña</legend><br>
+        <?php echo str_repeat('&nbsp', 5) . 'El usuario introducido es:' . str_repeat('&nbsp', 2) . $_POST["usuario"];?></br></br>
+        <?php echo str_repeat('&nbsp', 5) . 'La contraseña primera introducido es:' . str_repeat('&nbsp', 2) . $_POST["contraseña1"];?></br></br>
+        <?php echo str_repeat('&nbsp', 5) . 'La contraseña segunda introducido es:' . str_repeat('&nbsp', 2) . $_POST["contraseña2"];?></br></br>
+        <?php echo str_repeat('&nbsp', 5);
+        iguales($_POST["contraseña1"], $_POST["contraseña2"]);?></br></br>
+    </fieldset></br>
+    <fieldset>
+        <legend>Nombre, apellidos y correo</legend><br>
+        <?php echo str_repeat('&nbsp', 5) . 'El nombre introducido es:' . str_repeat('&nbsp', 2) . $_POST["nombre"];?></br></br>
+        <?php echo str_repeat('&nbsp', 5) . 'Los apellidos introducidas son:' . str_repeat('&nbsp', 2) . $_POST["apellidos"];?></br></br>
+        <?php echo str_repeat('&nbsp', 5) . 'El correo introducido es:' . str_repeat('&nbsp', 2) . $_POST["correo"];?></br></br>
+    </fieldset></br>
+    <fieldset>
+        <legend>Lista de coches</legend><br>
+        <?php echo str_repeat('&nbsp', 5) . 'Las marcas introducidas son:' . str_repeat('&nbsp', 2);
+        if(!validar('marcas', $_POST) == null){
+            echo prettyVardump($_POST["marcas"]);
+        } ?></br></br>
+    </fieldset>
